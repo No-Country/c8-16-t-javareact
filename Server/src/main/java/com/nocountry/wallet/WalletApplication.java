83fad29d5969a172c066bad2b67505d91ae05834
Brian Polesel <brianpolesel07@gmail.com>
2022-11-16 12:39:18 -0300

    First steps, now can login and take JWT

--- Server/src/main/java/com/nocountry/wallet/WalletApplication.java
+++ Server/src/main/java/com/nocountry/wallet/WalletApplication.java
@@ -2,6 +2,7 @@ package com.nocountry.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class WalletApplication {
--- Server/src/main/java/com/nocountry/wallet/utils/enumeration/ErrorEnum.java
+++ Server/src/main/java/com/nocountry/wallet/utils/enumeration/ErrorEnum.java
@@ -8,7 +8,11 @@ public enum ErrorEnum {
     TRANSACTION_LIMIT("You have exceeded the transaction limit"),
     REQUEST_FAILED("The request was not completed"),
     INSUFFICIENT_BALANCE("You need more balance for this payment"),
-    OBJECT_NOT_FOUND("The requested object was not found");
+    OBJECT_NOT_FOUND("The requested object was not found"),
+    USER_ALREADY_EXIST("User already exist in Database"),
+    EMPTY_PASS("The password is empty"),
+    INVALID_PASSWORD("Password provided is a invalid password"),
+    BAD_CREDENTIALS("Username or password is invalid");
 
     private String errorMessage;
 
