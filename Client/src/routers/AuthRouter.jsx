97dd52ac0dc168792245ec34b8ec27a25ddcc98f
sky <ant_664@hotmail.com>
2022-11-20 18:00:14 -0500

    fix: AuthRouter

--- Client/src/routers/AuthRouter.jsx
+++ Client/src/routers/AuthRouter.jsx
@@ -1,18 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import LogIn from './../pages/LogIn';
 import SignIn from './../pages/SignIn';
 
 const AuthRouter = () => {
   return (
-    <div>
+    <>
       <Routes>
         <Route path='/login' element={<LogIn />} />
         <Route path='/signin' element={<SignIn />} />
         {/* <Route path='/reset' element={<ResetPassword />} /> */}
       </Routes>
-      <Route path='/not_found' element={'Not Found'}/>
-      <Route path={'*'} element={<Navigate to='/not_found' replace />}/>
-    </div>
+    </>
   )
 }
 
