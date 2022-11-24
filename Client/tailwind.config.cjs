f76efc76ec836fa6790c991efdf78bfcfde890b5
Exequiel Schiavo <exelu1495@gmail.com>
2022-11-24 13:56:30 -0300

    feat:styles

--- Client/tailwind.config.cjs
+++ Client/tailwind.config.cjs
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      Grayexe: "#D9D9D9",
+      GreenPrimary: "#3CAF00",
+      GreenSecondary: "#018B02",
+      GrayPrimary: "#848484",
+      GraySecondary: "#CECECE",
+      Black: "#000000",
     },
 
     fontFamily: {
