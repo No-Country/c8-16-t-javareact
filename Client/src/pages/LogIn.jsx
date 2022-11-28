7867d42ec44cf684c79bf897a5d50315337f1a31
Franciscozcode <zzpanch@gmail.com>
2022-11-28 19:13:17 -0300

    update login 2 reunion

--- Client/src/pages/LogIn.jsx
+++ Client/src/pages/LogIn.jsx
@@ -22,13 +22,13 @@ const handleSubmit = () => {
 
   return (
     <div >
-      <div className=" flex flex-col items-center" >
-        LOGO
+      <div className="bg-lightGreen rounded-b-[50px] w-[360px] h-[151px] mx-auto text-3xl text-center" >
+        <p>LOGO</p>
         </div>
-        <form onSubmit={handleSubmit} className="flex flex-col items-center bg-blue-500">
-          <input value={values.email} name="email" id="email" type="email" placeholder="Ingresar e-mail" onChange={handleInputChange} />
-          <input value={values.password} name="password" id="password" type="password" placeholder="Ingrese su contraseña" onChange={handleInputChange} />
-          <button className="flex flex-col bg-blue-300 hover:bg-blue-500 font-bold py-2 px-4 rounded-full">Enviar</button>
+        <form onSubmit={handleSubmit} className=" my-4 flex flex-col items-center">
+          <input value={values.email} name="email" id="email" type="email" placeholder="Ingresar e-mail" onChange={handleInputChange} className="inputForm my-2 border-2 border-gray-600 " />
+          <input value={values.password} name="password" id="password" type="password" placeholder="Ingrese su contraseña" onChange={handleInputChange} className="inputForm my-2 border-2 border-gray-600" />
+          <button className="flex flex-col bg-green-300 hover:bg-green-500 font-bold my-4 py-2 px-4 rounded-full">Enviar</button>
 
         </form>
       
