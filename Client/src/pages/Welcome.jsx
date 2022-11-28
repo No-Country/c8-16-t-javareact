d78bb4559920ab1e8c1889d955b32d9b4ff69440
Exequiel Schiavo <exelu1495@gmail.com>
2022-11-28 19:01:20 -0300

    update : change shadow buttons

--- Client/src/pages/Welcome.jsx
+++ Client/src/pages/Welcome.jsx
@@ -2,22 +2,22 @@ import { Link } from "react-router-dom";
 
 const Welcome = () => {
   return (
-    <div className="container  flex flex-col justify-center items-center ">
-      <div className=" w-40 h-40 text-4xl bg-Grayexe rounded-full flex justify-center  items-center   mt-9 ">
+    <div className="container h-screen flex flex-col justify-start items-center  bg-gradient-to-t from-lightGreen to-darkGreen">
+      <div className=" w-40 h-40 text-4xl  rounded-full flex justify-center  items-center   mt-32 ">
         {" "}
-        Logo{" "}
+        <img src="/Group.svg" alt="logo" />
       </div>
 
-      <div className="flex gap-5 flex-col mt-28 font-Lato">
+      <div className="flex gap-5 flex-col mt-28 font-Raleway font-semibold  text-base">
         {" "}
         <Link to="auth/Login">
-          <div class="  border-none   text-black rounded- px-5 py-2 w-72 flex items-center justify-center">
+          <div class="  border-none  shadow-xl bg-white   text-darkGrey rounded-2xl px-5 py-2 w-72 flex items-center justify-center">
             {" "}
             Ingresar
           </div>
         </Link>
         <Link to="auth/Signin">
-          <div class="border-solid border-2 border-black  bg-white text-black rounded-2xl px-5 py-2 w-72 flex items-center justify-center">
+          <div class="border-solid border  border-white  text-white rounded-2xl px-5 py-2 w-72 flex items-center justify-center">
             {" "}
             Abrir cuenta gratis
           </div>
