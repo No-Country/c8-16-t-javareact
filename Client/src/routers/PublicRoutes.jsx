07955e9916dea7117676448d19921b32315a138c
guillermoPintos <96449430+guillermoPintos@users.noreply.github.com>
2022-12-01 16:06:27 -0300

    prueba

--- Client/src/routers/PublicRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const PublicRoutes = ({ children, isLogged }) => {
-
-  return isLogged ? <Navigate to='/app/home' /> : children;
-}
-
-export default PublicRoutes
