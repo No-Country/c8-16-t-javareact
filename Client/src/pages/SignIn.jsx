153f01cd0c93aee2f4e705a6581886d3bbac4136
MAXIMILIANO_SARMIENTO <maximiliano.sarmiento11@gmail.com>
2022-12-01 09:10:03 -0300

    styles updating, validations and errors on date and dni inputs

--- Client/src/pages/SignIn.jsx
+++ Client/src/pages/SignIn.jsx
@@ -42,23 +42,21 @@ const SignIn = () => {
     console.log(values);
   };
 
-  console.log(errors)
-
   return (
     <div className="w-[360px] mx-auto">
       <div className="bg-lightGreen rounded-b-[50px] w-full h-[151px] mx-auto flex">
-        <img className="mx-auto w-28" src={logo} alt="logo"/>
+        <img className="mx-auto w-28" src={logo} alt="logo" />
       </div>
 
-      <p className="w-[312px] mx-auto mt-[50px] mb-[20px] text-darkGrey">
+      <p className="w-[312px] mx-auto mt-[30px] mb-[40px] text-darkGrey">
         Para registrarte, ingresa los siguientes datos por favor
       </p>
 
       <form
         onSubmit={handleSubmit}
-        className=" w-[312px] mx-auto flex flex-col gap-y-[20px]"
+        className=" w-[312px] mx-auto flex flex-col gap-y-[10px]"
       >
-        <span>
+        <span id="emailContainer">
           <input
             onBlur={() => handleBlur("email")}
             value={values.email}
@@ -73,8 +71,10 @@ const SignIn = () => {
             <p className="errors">{errors.email}</p>
           )}
         </span>
-        <span>
+
+        <span id="dniContainer">
           <input
+            onBlur={() => handleBlur("dni")}
             value={values.dni}
             name="dni"
             id="dni"
@@ -83,17 +83,28 @@ const SignIn = () => {
             className="inputForm w-full"
             onChange={handleInputChange}
           />
+          {errors.dni && touched == "dni" && (
+            <p className="errors">{errors.dni}</p>
+          )}
         </span>
 
-        <input
-          value={values.birthdate}
-          name="birthdate"
-          id="birthdate"
-          type="date"
-          placeholder="Fecha de nacimiento"
-          className="inputForm w-full"
-          onChange={handleInputChange}
-        />
+        <span id="dateContainer">
+          <input
+            onBlur={() => {
+              handleBlur("email");
+            }}
+            value={values.birthdate}
+            name="birthdate"
+            id="birthdate"
+            type="date"
+            placeholder="Fecha de nacimiento"
+            className="inputForm w-full"
+            onChange={handleInputChange}
+          />
+          {errors.birthdate && touched == "birthdate" && (
+            <p className="errors">{errors.birthdate}</p>
+          )}
+        </span>
 
         <span className="flex w-full gap-x-[10px]">
           <span className="flex flex-col">
@@ -127,7 +138,8 @@ const SignIn = () => {
             )}
           </span>
         </span>
-        <span>
+
+        <span id="passwordContainer">
           <input
             onBlur={() => handleBlur("password")}
             value={values.password}
@@ -142,7 +154,8 @@ const SignIn = () => {
             <p className="errors">{errors.password}</p>
           )}
         </span>
-        <span>
+
+        <span id="confirmPasswordContainer">
           <input
             onBlur={() => handleBlur("confirmpassword")}
             value={values.confirmpassword}
@@ -160,7 +173,7 @@ const SignIn = () => {
 
         <button
           type="submit"
-          className="w-full h-[40px] bg-lightGreen text-white font-semibold rounded-[20px] mt-[77px] shadow-[1px_2px_4px_2px_rgba(0,0,0,0.25)]"
+          className="w-full h-[40px] bg-lightGreen text-white font-semibold rounded-[20px] mt-[27px] shadow-[1px_2px_4px_2px_rgba(0,0,0,0.25)]"
         >
           Confirmar
         </button>
