71432cf94195f71b34430348f312b41e77aab7be
MAXIMILIANO_SARMIENTO <maximiliano.sarmiento11@gmail.com>
2022-12-01 17:08:23 -0300

    add: profileBar component

--- Client/src/routers/AplicationRouter.jsx
+++ Client/src/routers/AplicationRouter.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Onboarding from "../pages/Onboarding";
 import VerifyPage from "../pages/VerifyPage";
+import ProfileBar from "../pages/ProfileBar";
 
 const AplicationRouter = () => {
   return (
@@ -9,6 +10,7 @@ const AplicationRouter = () => {
         <Route path="/onboarding" element={<Onboarding />} />
         <Route path="/profile" element={"Profile"} />
         <Route path="/verificateemail" element={<VerifyPage />} />
+        <Route path="/profilebar" element={<ProfileBar />} />
         {/* <Route path='/not_found' element={'Not Found'}/>
         <Route path={'/*'} element={<Navigate to='/not_found' replace />}/> */}
       </Routes>
