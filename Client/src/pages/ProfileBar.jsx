30ef7f5703f85634b30818b37575e5d0bff4e67f
MAXIMILIANO_SARMIENTO <maximiliano.sarmiento11@gmail.com>
2022-12-03 11:58:59 -0300

    feat: position fixed in profileBar

--- Client/src/pages/ProfileBar.jsx
+++ Client/src/pages/ProfileBar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const ProfileBar = () => {
     return (
-      <div className="containerProfileBar flex w-[360px] h-[51px] mx-auto bg-white shadow-[1px_2px_4px_2px_rgba(0,0,0,0.25)] items-center justify-between">
+      <div className="containerProfileBar flex w-[360px] h-[51px] mx-auto bg-white shadow-[1px_2px_4px_2px_rgba(0,0,0,0.25)] items-center justify-between fixed">
         <span className='w-11/12 flex items-center justify-between mx-auto'>
           <img src={logo} alt="logo" className='w-10'/>
           <Link to='app/profile' className='text-3xl text-lightGreen'><BiUserCircle/></Link>
