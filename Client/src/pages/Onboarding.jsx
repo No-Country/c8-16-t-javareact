86e6925eab1741c2c93fc2d7086a6a7e93252040
Franciscozcode <zzpanch@gmail.com>
2022-12-05 17:18:31 -0300

    Onboarding version lunes

--- Client/src/pages/Onboarding.jsx
+++ Client/src/pages/Onboarding.jsx
@@ -3,6 +3,8 @@ import { useState } from "react"
 import image from "/Group46.png"
 import image2 from "/Group49.png"
 
+
+
 const Onboarding = () => {
     const [estado, setEstado] = useState(0)
     
@@ -35,17 +37,24 @@ const Onboarding = () => {
     
     
     return(
-        <div className="w-[360px] mx-auto text-3xl text-center">
+        <div className="flex  w-[360px] h-[151px] mx-auto  ">
         <div>
-            <div className="bg-lightGreen rounded-b-[50px] w-[360px] h-[151px]" >
-        <p>LOGO</p>
+            <div className="bg-lightGreen rounded-b-[50px] w-[360px] h-[151px] mx-auto text-3xl text-center" >
+            <img className="mx-auto" src="/Group.svg" alt="logo" />
+        
+
         </div>
-            <img src={data[estado].img} alt="imagen" />
-            <p >{data[estado].titulo}</p>            
-            <p>{data[estado].parrafo}</p>
+        <div>
+        <h2 className="text-lightGreen font-raleway  text-xl font-semibold text-center py-4 px-4">Bienvenid@</h2>
+
+        </div>
+            
+            <img className="mx-auto py-2 px-4" src={data[estado].img} alt="imagen" />
+            <p className="text-lightGreen font-semibold text-base py-2 px-4">{data[estado].titulo}</p>            
+            <p className="mx-auto py-2 px-4">{data[estado].parrafo}</p>
             <button></button>
             <button></button>
-            <button onClick={handledClick}>SIGUIENTE</button>
+            <button className="py-4 px-4" onClick={handledClick}>SIGUIENTE</button>
         </div>
         </div>
     )
