0a158c00e026f324ff5c1f0a428a27070f14f352
guillermoPintos <96449430+guillermoPintos@users.noreply.github.com>
2022-12-08 19:42:49 -0300

    otp fix

--- Server/src/main/java/com/nocountry/wallet/service/impl/EmailServiceImpl.java
+++ Server/src/main/java/com/nocountry/wallet/service/impl/EmailServiceImpl.java
@@ -70,7 +70,7 @@ public class EmailServiceImpl implements EmailService {
         Email toEmail = new Email(emailTo);
         otpCode = otpUtils.otpStarter();
         Content content = new Content("text/html", "El codigo de verificacion es: "+ getOtpCode());
-        String subject = "CODIGO DE VERIFICACION CUENTA FLUX";
+        String subject = otpCode;
         Mail mail = new Mail(fromEmail, subject, toEmail, content);
         //mail.setTemplateId(templateId);
         SendGrid sg = new SendGrid(sendGridKey);
